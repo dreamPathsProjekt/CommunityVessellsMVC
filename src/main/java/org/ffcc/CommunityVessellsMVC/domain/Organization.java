94e7dd6cf42b8e69ed4e1a1317210ca1ad6d736b
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2017-07-19 19:59:53 +0300

    Add Domain Objects

---
+++ src/main/java/org/ffcc/CommunityVessellsMVC/domain/Organization.java
@@ -0,0 +1,102 @@
+package org.ffcc.CommunityVessellsMVC.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Organization {
+	
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(unique=true)
+	@Email(message = "*Please provide a valid Email")
+	@NotEmpty(message = "*Please provide an email")
+	private String email;
+	
+	
+	@Length(min = 8, message = "*Your password must have at least 8 characters")
+	@NotEmpty(message = "*Please provide your password")
+	@JsonIgnore	
+	private String password;
+	
+	private String avatarPath;
+	private String description;
+	private String type;
+	
+	@OneToMany(mappedBy = "organization")
+	private List<Event> events;
+
+	
+	
+	public Organization() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAvatarPath() {
+		return avatarPath;
+	}
+
+	public void setAvatarPath(String avatarPath) {
+		this.avatarPath = avatarPath;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+	
+	
+	
+}
