829e5f40b92704bc42b69555864e5388d4c8f968
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2017-07-19 16:44:15 +0300

    Initial Commit

---
+++ src/main/java/org/ffcc/CommunityVessellsMVC/CommunityVessellsMvcApplication.java
@@ -0,0 +1,12 @@
+package org.ffcc.CommunityVessellsMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityVessellsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityVessellsMvcApplication.class, args);
+	}
+}
